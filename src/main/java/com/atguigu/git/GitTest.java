c8187723c640fbeda46df32a53177aba31c0bde0
Ker <397057084@qq.com>
2022-06-04 11:47:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wckmove
+ * @create 2022-06-04 11:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
